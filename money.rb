50e6466e107868db8dfb3c938de4572fe97087bf
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-10-11 16:46:35 +0200

    Put the Money module back

--- money.rb
+++ money.rb
@@ -1,22 +1,26 @@
-class Money
-  def initialize(amount, currency)
-    @amount = amount
-    @currency = currency
+module Money
+
+  class Money
+    def initialize(amount, currency)
+      @amount = amount
+      @currency = currency
+    end
+
+    def times(n)
+      return self.class.new(amount * n, currency)
+    end
+
+    def plus(other)
+      self.class.new(amount + other.amount, currency)
+    end
+
+    def ==(other)
+      amount == other.amount && self.currency == other.currency
+    end
+
+    protected
+
+    attr_reader :amount, :currency
   end
 
-  def times(n)
-    return self.class.new(amount * n, currency)
-  end
-
-  def plus(other)
-    self.class.new(amount + other.amount, currency)
-  end
-
-  def ==(other)
-    amount == other.amount && self.currency == other.currency
-  end
-
-  protected
-
-  attr_reader :amount, :currency
 end
--- money_spec.rb
+++ money_spec.rb
@@ -1,21 +1,25 @@
 require 'rspec'
 require_relative 'money'
 
-describe Money do
-  it 'compares amounts of different currencies' do
-    Money.new(5, :dollar).should == Money.new(5, :dollar)
-    Money.new(5, :dollar).should_not == Money.new(6, :dollar)
-    Money.new(5, :dollar).should_not == Money.new(5, :euro)
+module Money
+
+  describe Money do
+    it 'compares amounts of different currencies' do
+      Money.new(5, :dollar).should == Money.new(5, :dollar)
+      Money.new(5, :dollar).should_not == Money.new(6, :dollar)
+      Money.new(5, :dollar).should_not == Money.new(5, :euro)
+    end
+
+    it 'allows multiplication' do
+      Money.new(5, :dollar).times(2).should == Money.new(10, :dollar)
+    end
+
+    it 'allows addition' do
+      five = Money.new( 5, :dollar)
+      ten  = Money.new(10, :dollar)
+
+      five.plus(five).should == ten
+    end
   end
 
-  it 'allows multiplication' do
-    Money.new(5, :dollar).times(2).should == Money.new(10, :dollar)
-  end
-
-  it 'allows addition' do
-    five = Money.new( 5, :dollar)
-    ten  = Money.new(10, :dollar)
-
-    five.plus(five).should == ten
-  end
 end
