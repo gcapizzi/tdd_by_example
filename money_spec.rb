9833ac36ef4745e5c34a40c4ec97d70dea1a0571
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-10-11 18:05:07 +0200

    Specs refactoring

--- money_spec.rb
+++ money_spec.rb
@@ -1,28 +1,33 @@
 require 'rspec'
 require_relative 'money'
 
+class Fixnum
+  def money(currency); Money::Money.new(self, currency); end
+  def dollars; money(:dollar); end
+  def euros; money(:euro); end
+end
+
 module Money
 
   describe Money do
     it 'compares amounts of different currencies' do
-      Money.new(5, :dollar).should == Money.new(5, :dollar)
-      Money.new(5, :dollar).should_not == Money.new(6, :dollar)
-      Money.new(5, :dollar).should_not == Money.new(5, :euro)
+      5.dollars.should == 5.dollars
+      5.dollars.should_not == 6.dollars
+      5.dollars.should_not == 5.euros
     end
 
     describe '#times' do
       it 'multiplies money by a constant' do
-        Money.new(5, :dollar).times(2).should == Money.new(10, :dollar)
+        5.dollars.times(2).should == 10.dollars
       end
     end
 
     describe '#plus' do
       it 'returns a Sum object' do
-        five = Money.new( 5, :dollar)
-        sum = five.plus(five)
+        sum = 5.dollars.plus(5.dollars)
 
-        sum.augend.should == five
-        sum.addend.should == five
+        sum.augend.should == 5.dollars
+        sum.addend.should == 5.dollars
       end
     end
   end
@@ -30,8 +35,8 @@ module Money
   describe Sum do
     describe '#reduce' do
       it 'adds the two addends' do
-        sum = Sum.new(Money.new(3, :dollar), Money.new(4, :dollar))
-        sum.reduce(:dollar).should == Money.new(7, :dollar)
+        sum = Sum.new(3.dollars, 4.dollars)
+        sum.reduce(:dollar).should == 7.dollars
       end
     end
   end
