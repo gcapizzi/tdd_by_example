8eed86a44c7ddfb1699d384fbee6e3ce1c3d336b
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-10-11 16:32:59 +0200

    Remove Money module

--- money.rb
+++ money.rb
@@ -1,22 +1,18 @@
-module Money
-
-  class Money
-    def initialize(amount, currency)
-      @amount = amount
-      @currency = currency
-    end
-
-    def times(n)
-      return self.class.new(amount * n, currency)
-    end
-
-    def ==(other)
-      amount == other.amount && self.currency == other.currency
-    end
-
-    protected
-
-    attr_reader :amount, :currency
+class Money
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
   end
 
+  def times(n)
+    return self.class.new(amount * n, currency)
+  end
+
+  def ==(other)
+    amount == other.amount && self.currency == other.currency
+  end
+
+  protected
+
+  attr_reader :amount, :currency
 end
--- money_spec.rb
+++ money_spec.rb
@@ -1,18 +1,14 @@
 require 'rspec'
 require_relative 'money'
 
-module Money
-
-  describe Money do
-    it 'compares amounts of different currencies' do
-      Money.new(5, :dollar).should == Money.new(5, :dollar)
-      Money.new(5, :dollar).should_not == Money.new(6, :dollar)
-      Money.new(5, :dollar).should_not == Money.new(5, :euro)
-    end
-
-    it 'allows multiplication' do
-      Money.new(5, :dollar).times(2).should == Money.new(10, :dollar)
-    end
+describe Money do
+  it 'compares amounts of different currencies' do
+    Money.new(5, :dollar).should == Money.new(5, :dollar)
+    Money.new(5, :dollar).should_not == Money.new(6, :dollar)
+    Money.new(5, :dollar).should_not == Money.new(5, :euro)
   end
 
+  it 'allows multiplication' do
+    Money.new(5, :dollar).times(2).should == Money.new(10, :dollar)
+  end
 end
